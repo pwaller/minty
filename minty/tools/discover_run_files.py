e8e6274c9f34640f333924386627c0ed3b01b1ce
Peter Waller <peter.waller@cern.ch>
2010-09-16 13:04:34 +0200

    Add discover_run_files and merge_runs.py

---
+++ minty/tools/discover_run_files.py
@@ -0,0 +1,40 @@
+#! /usr/bin/env python
+"""
+Outputs a list of filenames which contain each run
+"""
+
+# Scripts expect to be run with a cwd where "minty" and "pytuple" are present.
+# (Or minty and pytuple are in the path)
+import os; import sys; sys.path.insert(0, os.getcwd())
+
+from minty import main, AnalysisBase
+from logbook import Logger; log = Logger("RunDiscoverer")
+
+def record_run(ana, event):
+    """
+    Record a list of {runnumber : [filenames]}
+    """
+    this_tree, this_run = event.tree.GetTree(), event.RunNumber
+    if ana.last_tree != this_tree or ana.last_run != this_run:
+        ana.last_tree, ana.last_run = this_tree, this_run
+        this_file = this_tree.GetDirectory().GetName()
+        ana.runs.setdefault(this_run, []).append(this_file)
+
+class RunDiscoverer(AnalysisBase):
+    def __init__(self, tree, options):    
+        super(RunDiscoverer, self).__init__(tree, options)
+        
+        self.last_tree = self.last_run = None
+        self.runs = {}
+        
+        self.tasks.extend([
+            record_run,
+        ])
+
+    def finalize(self):
+        log.info("Completed")
+        for run, files in sorted(self.runs.iteritems()):
+            print run, " ".join(sorted(files))
+
+if __name__ == "__main__":
+    main(RunDiscoverer)
---
+++ minty/tools/merge_runs.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+# Scripts expect to be run with a cwd where "minty" and "pytuple" are present.
+# (Or minty and pytuple are in the path)
+import os; import sys; sys.path.insert(0, os.getcwd())
+
+from minty.utils.hadd import merge_all
+
+def main():
+    with open("run_files") as fd:
+        runs = [line.strip().split() for line in fd if line.strip()]
+    
+    for run in runs:
+        run_no, files = run[0], run[1:]
+        print "Merging run %s (%i files)" % (run_no, len(files))
+        merge_all("mike-data-merged/%s.root" % run_no, files)
+        
+if __name__ == "__main__":
+    main()
