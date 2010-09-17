8a3734eabba461683248df6b265f9a5d753980d3
Peter Waller <peter.waller@cern.ch>
2010-09-17 14:53:50 +0200

    Add counts code

---
+++ analyses/post-grid/counts.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+
+# Scripts expect to be run with a cwd where "minty" and "pytuple" are present.
+# (Or minty and pytuple are in the path)
+import os; import sys; sys.path.insert(0, os.getcwd())
+
+from IPython.Shell import IPShellEmbed; ip = IPShellEmbed(["-pdb"])
+
+from minty.histograms.cuts_histogram import make_cut_histogram
+import ROOT as R
+
+def print_stats(axis):
+    print "%12s" % axis.title, "total=%6i" % axis.true,
+    proj = axis.project_out(True)
+    
+    counts = []
+    for ax in ["grl", "pv", "g10_loose", "oq"]:
+        counts.append("%s=%6i" % (ax, proj(ax).true))
+        
+    combined = ["grl"]
+    for axis in ["g10_loose", "pv", "oq"]:
+        combined.append(axis)
+        combined_axis = proj(*combined)
+        counts.append("%s=%6i" % (combined_axis.title, combined_axis.true))
+        #if len(combined) == 4:ip()
+        
+    print " ".join(counts)
+      
+def test_run(input_file):
+    
+    f = R.TFile(input_file)   
+    h_all = make_cut_histogram(f.photon_counts)
+        
+    print input_file
+    
+    h = h_all.fiducial.project_out(True)
+    print_stats(h.loose)
+    print_stats(h.tight)    
+    print_stats(h.robust_tight)
+    
+    print "Non-fiducial photons:"
+    h = h_all.fiducial.project_out(False)
+    print_stats(h.loose)
+    print_stats(h.tight)    
+    print_stats(h.robust_tight)
+    
+test_run("result/160980.root")
+#test_run("new.root")
---
+++ minty/histograms/cuts_histogram.py
@@ -0,0 +1,172 @@
+"""
+A cuts histogram is one which encodes information about correlations between all 
+cuts simultaneously.
+"""
+
+import ROOT as R
+
+R.THnSparse.GetDimension = R.THnSparse.GetNdimensions
+
+def TH1__GetAxis(self, i):
+    assert i <= 3
+    return [R.TH1.GetXaxis, R.TH1.GetYaxis, R.TH1.GetZaxis][i](self)
+R.TH1.GetAxis = TH1__GetAxis
+
+from array import array
+from sys import stdout
+from itertools import izip_longest
+from os import listdir
+
+class CutAxis(object):
+    def __init__(self, cuthisto, *titles):
+        #print "New cutaxis:", cuthisto, titles
+        self.cuthisto = cuthisto
+        self.titles = titles
+        self._projected = None
+    
+    @property
+    def projected(self):
+        if not self._projected:
+            self._projected = self.cuthisto.project(*self.idxs)
+        return self._projected
+
+    @property
+    def title(self):
+        return "+".join(self.titles)
+
+    def find_bin(self, *values):
+        h = self.projected.hist
+        
+        #return h.GetBin(array("i", values))
+        if len(self.titles) > 3:
+            return h.GetBin(array("i", values))
+        else:
+            return h.FindBin(*values)
+
+    def project_out(self, *slices):
+        """
+        Remove this axis from the target histogram
+        """
+        # Keep track of previous axis states before projection
+        set_axes = [] 
+        
+        for slice, axis in izip_longest(slices, self.axes):
+            if isinstance(slice, (int, long)):
+                slice = self.find_bin(slice)
+                bin_range = slice, slice
+            elif isinstance(slice, tuple):
+                lo, hi = slice
+                bin_range = self.find_bin(lo), self.find_bin(hi)
+            elif slice is None:
+                bin_range = None
+            else:
+                assert False, "Invalid slice"
+            
+            if bin_range:
+                prev_set = axis.TestBit(R.TAxis.kAxisRange)
+                prev_range = axis.GetFirst(), axis.GetLast()
+                set_axes.append((axis, prev_set, prev_range))
+                axis.SetRange(*bin_range)
+                #axis.SetBit(R.TAxis.kAxisRange)
+            
+        idxs = set(self.idxs)
+        axes = [i for i in xrange(self.cuthisto.hist.GetDimension()) 
+                if i not in idxs]
+        result = self.cuthisto.project(*axes)
+        for axis, axis_range_bit, prev_range in set_axes:
+            axis.SetRange(*prev_range)
+            axis.SetBit(R.TAxis.kAxisRange, axis_range_bit) 
+        return result
+    
+    @property
+    def axes(self):
+        return map(self.cuthisto.axis, self.titles)
+    
+    @property
+    def idxs(self):
+        #print self.titles
+        return map(self.cuthisto.axis_idx, self.titles)
+    
+    def value(self, *position): 
+        return self.find_bin(*position)
+    
+    @property
+    def true(self):
+        return self.projected[self.find_bin(*(True,)*len(self.titles))]
+        
+    @property
+    def false(self):
+        return self.projected[self.find_bin(*(False,)*len(self.titles))]
+    
+    def __repr__(self):
+        args = self.titles, self.false, self.true
+        return "<CutAxis %s false=%i true=%i>" % args
+        
+    def __and__(self, rhs):
+        return CutAxis(self.cuthisto, *(self.titles + rhs.titles))
+        
+def make_cut_histogram(hist, axestitles=None):
+    class CutHistogram_Specific(CutHistogram):
+        pass
+    
+    if isinstance(hist, R.TH1):
+        CutHistogram_Specific.__getitem__ = lambda self, i: hist[i]
+    
+    if axestitles:
+        assert len(axestitles) == hist.GetDimension()
+        for i, axistitle in enumerate(axestitles):
+            hist.GetAxis(i).SetTitle(axistitle)
+    
+    axis_idxs = {}
+    axes = []
+    for i in xrange(hist.GetDimension()):
+        title = hist.GetAxis(i).GetTitle()
+        get_cutaxis = lambda self, n=title: self(n)
+        setattr(CutHistogram_Specific, title, property(get_cutaxis))
+        axes.append(title)
+        axis_idxs[title] = i
+        #print title, i
+
+    return CutHistogram_Specific(hist, axes, axis_idxs)
+    
+class CutHistogram(object):
+    def __init__(self, hist, axes, axis_idxs):
+        self.hist = hist
+        self.axes = axes
+        self.axis_idxs = axis_idxs 
+    
+    def __call__(self, *what):
+        return CutAxis(self, *what)
+        
+    def axis_idx(self, title):
+        return self.axis_idxs[title]
+    
+    def axis(self, title):
+        return self.hist.GetAxis(self.axis_idx(title))
+    
+    def __getitem__(self, what):
+        return self.hist.GetBinContent(what)
+    
+    def project(self, *axes):
+        assert len(axes) < len(self.axes)
+        
+        """
+        if len(self.axes) == 3:
+            new_h = self.hist.Project3D("".join("xyz"[i] for i in axes))
+            new_h.SetDirectory(None)
+        elif len(self.axes) == 2:
+            assert len(axes) == 1
+            do_proj = [self.hist.ProjectionX, self.hist.ProjectionY][axes[0]]
+            new_h = do_proj()
+            new_h.SetDirectory(None)
+        """
+        
+        if len(axes) <= 3:
+            new_h = self.hist.Projection(*axes)
+            new_h.SetDirectory(None)
+        else:
+            new_h = self.hist.Projection(len(axes), array("i", axes))
+        
+        #print "Projected:", new_h, new_h.GetName()
+        #R.gDirectory.ls()
+        return make_cut_histogram(new_h)
