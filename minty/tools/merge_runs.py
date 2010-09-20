58175e89e2246ee20bb013a17e412324965e3680
Peter Waller <peter.waller@cern.ch>
2010-09-20 14:05:52 +0200

    Parameterize merge runs script

--- minty/tools/merge_runs.py
+++ minty/tools/merge_runs.py
@@ -6,14 +6,17 @@ import os; import sys; sys.path.insert(0, os.getcwd())
 
 from minty.tools.hadd import merge_all
 
+MERGE_LIST, INPUT_PATH = "run_files", "mike-data-merged"
+MERGE_LIST, INPUT_PATH = "mike_runs_2_skim", "mike-data-merged-2"
+
 def main():
-    with open("run_files") as fd:
+    with open(MERGE_LIST) as fd:
         runs = [line.strip().split() for line in fd if line.strip()]
     
     for run in runs:
         run_no, files = run[0], run[1:]
         print "Merging run %s (%i files)" % (run_no, len(files))
-        merge_all("mike-data-merged/%s.root" % run_no, files, 
+        merge_all(INPUT_PATH + "/%s.root" % run_no, files, 
                   selection="e.Run == %s" % run_no)
         
 if __name__ == "__main__":
