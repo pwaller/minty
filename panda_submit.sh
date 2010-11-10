0dbc25a2ac79a8047c0d9a679a28b609f2449d8d
Peter Waller <pwaller@cern.ch>
2010-11-10 07:59:12 +0100

    Pass 12. ANALY_TOKYO seems to be the least flaky site, let's go there. Also adjusting maxgbperjob to 5.

--- panda_submit.sh
+++ panda_submit.sh
@@ -5,24 +5,23 @@ source /afs/cern.ch/atlas/offline/external/GRID/DA/panda-client/latest/etc/panda
 BADFILES=$(find . -wholename "*/.git/*" -or -iname "*.eps" -or -iname "*.png" | 
             cut -d'/' -f2- | xargs | sed -r 's/ /,/g')
 
-BADFILES=.git,\*.png,\*.eps
+BADFILES=.git,\*.png,\*.eps,.hg
 
-PASS=11
+PASS=12
 
 echo {A..I} |
 tr ' ' $'\n' | 
-xargs -I{} -n1 -P8 prun                                                         \
+xargs -I{} -n1 -P10 prun                                                         \
     --inDS group10.phys-sm.data10_7TeV.period{}.NoGRL.NTUP_PROMPTPHOT.p231/      \
     --outDS user.PeterWaller.purity.paudata.good.period.A.to.I.pass.$PASS.period{}/ \
     --noBuild                                                                    \
     --outputs output.root                                                        \
-    --nGBPerJob=MAX                                                              \
+    --nGBPerJob=5                                                              \
     --writeInputToTxt=IN:inputs.txt                                              \
     --excludeFile=input.txt,$BADFILES                                           \
     --extFile=$(ls minty/external/OQMaps/*.root | xargs echo | sed 's/ /,/g') \
     --exec './run_analysis.py analyses.purity PurityAnalysis -Ggrls/official inputs.txt' \
     --tmpDir /tmp/pwaller/pass.$PASS.period{}/                                 \
-    --excludedSite=ANALY_GRIF-LPNHE \
-    --excludedSite=ANALY_INFN-MILANO-ATLASC \
+    --site=ANALY_TOKYO \
     --athenaTag=16.0.0                                                           \
     $@ 
