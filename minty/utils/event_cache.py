c89629abd91479acb3117e5251917beccf1ce2cf
Peter Waller <peter.waller@cern.ch>
2010-11-29 05:17:54 +0100

    Remove beaker dependency

--- minty/utils/event_cache.py
+++ minty/utils/event_cache.py
@@ -1,21 +1,22 @@
-from beaker.cache import CacheManager
-from beaker.util import func_namespace
 
 class EventCache(object):
     def __init__(self):
-        self.manager = CacheManager()
-        self.cache = self.manager.get_cache("event_cache")
+        self.store = {}
+
+    def get_value(self, key, createfunc):
+        if not key in self.store:
+            self.store[key] = createfunc()
+        return self.store[key]
         
     def __call__(self, func):
         return func
         namespace = (func_namespace(func),)
-        cache = self.cache
         def trampoline(*args):
             key = namespace + tuple(hash(x) for x in args)
-            return cache.get_value(key, createfunc=lambda: func(*args))
+            return self.get_value(key, createfunc=lambda: func(*args))
         return trampoline
         
     def invalidate(self):
-        self.cache.clear()
+        self.store.clear()
         
 event_cache = EventCache()
