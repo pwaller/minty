b1837a1d6b28cb3601322ea710ca265cf6b4d97e
Peter Waller <pwaller@cern.ch>
2010-12-03 14:23:45 +0100

    setup.py Remove beaker requirement and add entry points for hmerge, show_ttree and ip_tfile

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="minty",
     packages=find_packages(),
-    install_requires=[
-        "beaker>=1.5.4"
-    ],
     entry_points={
         "console_scripts" : [
             "py_rep = minty.tools.repeat:main",
+            "show_ttree = minty.tools.show_ttree:main",
+            "ip_tfile = minty.tools.ipython_file:main",
+            "hmerge = minty.tools.bettermerge:main",
         ]
     }
 )
