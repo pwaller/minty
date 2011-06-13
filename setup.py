d6f276089c53b4bb7641380bcbad771a721257ad
Peter Waller <peter.waller@cern.ch>
2011-06-13 17:09:51 +0100

    Big cleanup. Remove analysis-specific things.

--- setup.py
+++ setup.py
@@ -6,14 +6,12 @@ setup(
     package_data={'': ["*.yaml"]},
     entry_points={
         "console_scripts" : [
-            "py_rep = minty.tools.repeat:main",
-            "show_ttree = minty.tools.show_ttree:main",
-            "ip_tfile = minty.tools.ipython_file:main",
-            "hmerge = minty.tools.bettermerge:main",
+            "minty-py-rep = minty.tools.repeat:main",
+            "minty-show-ttree = minty.tools.show_ttree:main",
+            "minty-ip-tfile = minty.tools.ipython_file:main",
             "minty-rescale = minty.tools.minty_rescale:main",
             "minty-xs = minty.tools.minty_xsection:main",
             "minty-runperiod-mapping = minty.metadata.period:main",
         ]
     },
-    scripts=["scripts/ds_number_to_what"],
 )
