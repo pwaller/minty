bdb40734ed588a838ab87cfabfcd091d6dcac8d1
Peter Waller <peter.waller@cern.ch>
2011-06-13 15:29:13 +0100

    Change Dataset type to be smarter

--- minty/metadata/ami.py
+++ minty/metadata/ami.py
@@ -5,9 +5,11 @@ amiCommand BrowseSQLQuery -sql="SELECT dataset.* FROM dataset WHERE dataset.logi
 
 #ListDataset predicate="logicalDatasetName LIKE 'data11_7TeV.00%.physics_Egamma.merge.NTUP_PHOTON.%_p555%'" project=data11_001 processingStep=real_data select=logicalDatasetName,nfiles,totalevents
 
-# Hack to prevent Ft module from overriding showwarnings with something broken
-# (imported by pyAMI)
+import yaml
+
 def get_amiclient():
+    # Hack to prevent Ft module from overriding showwarnings with something broken
+    # (imported by pyAMI)
     from warnings import showwarning
     showwarning.__module__ = "Ft"
 
@@ -16,31 +18,63 @@ def get_amiclient():
     amiclient = AMI(False)
     return amiclient
 
-class Dataset(object):
+yaml.add_representer(unicode, 
+    lambda dumper, value: dumper.represent_scalar(u'tag:yaml.org,2002:str', value))
+
+class Dataset(yaml.YAMLObject):
+    yaml_tag = u'!Atlas.Dataset'
+    
+    def __init__(self, contents):
+        def c(value):
+            if value.isdigit():
+                return int(value)
+            return value
+    
+        self.contents = dict((k.lower(), c(v)) for k, v in contents.iteritems())
+        
+    def __getattr__(self, what):
+        if what == "contents":
+            return {}
+        if what not in self.contents:
+            raise AttributeError(what)
+        return self.contents[what]
+    
+    @property
+    def name(self):
+        return self.logicaldatasetname
+    
+    @property
+    def status(self): 
+        return self.prodsysstatus
+        
+    @property
+    def good(self): 
+        return self.status == "EVENTS_AVAILABLE" and self.amistatus == "VALID"
+    
+    @property
+    def events(self):
+        return self.totalevents
+    
+    @property
+    def run(self):
+        return self.runnumber if "runnumber" in self.contents else self.datasetnumber
+    
+    @property
+    def period(self):
+        if self.projectname.startswith("mc"):
+            return "MC"
+        if self.contents.get("period", None):
+            return self.contents["period"]
+        return "UNK"
+    
     def __repr__(self):
         return ("<Dataset run={s.period}.{s.run} status=({s.status}, {s.amistatus}) "
                 "events={s.events} name={s.name}>").format(s=self)
 
 def make_dataset(x):
-    d = Dataset()
-    for key, value in x.iteritems():
-        if not value:
-            # Don't bother with empty attributes
-            continue
-        if value.isdigit():
-            value = int(value)
-        setattr(d, key.lower(), value)
-    d.status = d.prodsysstatus
-    d.good = d.status == "EVENTS_AVAILABLE" and d.amistatus == "VALID"
+    d = Dataset(x)
     if not d.good:
         return None
-    d.run = d.runnumber if hasattr(d, "runnumber") else d.datasetnumber
-    d.period = getattr(d, "period", "UNK")
-    if d.projectname.startswith("mc"):
-        d.period = "MC"
-    d.name = d.logicaldatasetname
-    d.files = d.nfiles
-    d.events = d.totalevents
     return d    
 
 def query_datasets(pattern):
